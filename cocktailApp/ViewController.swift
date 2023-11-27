fb1b16c21dbb1b239ac79f9f63a0e5e2703778b1
Djordje <stefanovic.djordje1997@gmail.com>
2023-11-27 12:18:31 +0100

    Initial commit

---
+++ cocktailApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cocktailApp
+//
+//  Created by Djordje Stefanovic on 11/27/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
