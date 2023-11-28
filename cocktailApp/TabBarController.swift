393edd84800c44c2b119ad46669a748375004aa1
Djordje <stefanovic.djordje1997@gmail.com>
2023-11-28 16:53:53 +0100

    CTL9-5: Set the values of the image and title of bar item in the TabBarController

--- cocktailApp/TabBarController.swift
+++ cocktailApp/TabBarController.swift
@@ -19,28 +19,24 @@ class TabBarController: UITabBarController {
         tabBar.layer.borderWidth = 0.5
         tabBar.layer.borderColor = UIColor.gray.cgColor
         tabBar.unselectedItemTintColor = UIColor.white.withAlphaComponent(0.5)
+        
         if let item1 = tabBar.items?[0] {
+            item1.image = UIImage(systemName: "wineglass")
+            item1.title = "Cocktails"
             item1.selectedImage = UIImage(systemName: "wineglass.fill")
         }
         
         if let item2 = tabBar.items?[1] {
+            item2.image = UIImage(systemName: "heart")
+            item2.title = "Favorites"
             item2.selectedImage = UIImage(systemName: "heart.fill")
         }
         
         if let item3 = tabBar.items?[2] {
+            item3.image = UIImage(systemName: "person")
+            item3.title = "Profile"
             item3.selectedImage = UIImage(systemName: "person.fill")
         }
-        
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
